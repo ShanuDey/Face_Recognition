cbbbe51d8e643bc2ebc96720bb4778ac0a632032
Shanu Dey <shanudey1999@gmail.com>
2019-04-06 11:46:54 +0530

    Updated

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-classifer = cv2.CascadeClassifier('Classifier/haarcascade_frontalface_default.xml')
+classifer = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
